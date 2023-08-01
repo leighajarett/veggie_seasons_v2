5e5ce8f2bce3fffe4c856fcfb4ae938409f994a1
Leigha Jarett <leigha.jarett@gmail.com>
2023-08-01 14:24:43 -0400

    Small changes

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:veggie_seasons_v2/components/Header.dart';
 import 'package:veggie_seasons_v2/data/veggie.dart';
 import 'package:veggie_seasons_v2/data/veggie_data.dart';
 import 'package:veggie_seasons_v2/styles/styles.dart';
@@ -80,71 +81,5 @@ class _HomeState extends State<Home> {
         ),
       ]),
     );
-  }
-}
-
-// Future -- it would be cool to have some "overscrolling"
-class CustomSliverHeaderDelegate extends SliverPersistentHeaderDelegate {
-  CustomSliverHeaderDelegate({required this.monthYear});
-
-  final String monthYear;
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    final progress = shrinkOffset / (maxExtent - minExtent);
-
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        Container(
-          color: $styles.colors.background,
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: $styles.padding.l),
-            child: Padding(
-              padding: EdgeInsets.only(bottom: $styles.padding.s),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    progress < 1
-                        ? AnimatedOpacity(
-                            duration: const Duration(milliseconds: 200),
-                            opacity: progress < 0.7 ? 1.0 : 0.0,
-                            child: Padding(
-                              padding:
-                                  EdgeInsets.only(bottom: $styles.padding.xs),
-                              child: Text(
-                                monthYear,
-                                style: $styles.text.subheading1
-                                    .copyWith(color: $styles.colors.black),
-                              ),
-                            ))
-                        : const SizedBox.shrink(),
-                    AnimatedDefaultTextStyle(
-                        duration: const Duration(milliseconds: 200),
-                        style: progress < 1
-                            ? $styles.text.heading1
-                                .copyWith(color: $styles.colors.black)
-                            : $styles.text.heading2
-                                .copyWith(color: $styles.colors.black),
-                        child: const Text("In season today")),
-                  ]),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  @override
-  double get maxExtent => 150;
-
-  @override
-  double get minExtent => kToolbarHeight + 44;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
-    return true;
   }
 }
